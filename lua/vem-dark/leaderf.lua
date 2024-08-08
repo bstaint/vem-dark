26e48e522a1586b88b1981eaee374c21d35f5af1
bstaint <bstaint@gmail.com>
2024-08-09 03:01:37 +0800

    fix(leaderf): improve leaderf color.

--- lua/vem-dark/leaderf.lua
+++ lua/vem-dark/leaderf.lua
@@ -3,8 +3,8 @@ local M = {}
 local function StlPalette()
   return {
     stlName = {
-      guifg = '#808080',
-      guibg = '#303030',
+      guifg = 'NvimLightGrey2',
+      guibg = '#303030'
     },
     stlCategory = {
       gui = 'bold',
@@ -14,22 +14,22 @@ local function StlPalette()
     stlNameOnlyMode = {
       gui = 'bold',
       guifg = '#373737',
-      guibg = '#cae682',
+      guibg = '#8ac6f2',
     },
     stlFullPathMode = {
       gui = 'bold',
       guifg = '#373737',
-      guibg = '#cae682',
+      guibg = '#8ac6f2',
     },
     stlFuzzyMode = {
       gui = 'bold',
       guifg = '#373737',
-      guibg = '#cae682',
+      guibg = '#8ac6f2',
     },
     stlRegexMode = {
       gui = 'bold',
       guifg = '#373737',
-      guibg = '#cae682',
+      guibg = '#8ac6f2',
     },
     stlCwd = {
       guifg = '#404040',
@@ -57,17 +57,17 @@ M.apply = function()
 
   hi('Lf_hl_selection', { link = 'Search' })
   hi('Lf_hl_cursorline', { link = 'CursorLine' })
-  hi('Lf_hl_popup_inputText', { fg = '#e6e3d8',  bg = '#242424' })
+  hi('Lf_hl_popup_inputText', { fg = 'NvimLightGrey2',  bg = '#242424' })
   hi('Lf_hl_popup_prompt', { link = 'Label' })
   hi('Lf_hl_popup_lineInfo', { fg = '#101010', bg = '#d7ff87' })
   hi('Lf_hl_popup_total', { fg = '#101010', bg = '#cae682' })
   hi('Lf_hl_popup_normalMode', { fg = '#101010', bg = '#cae682', bold = true })
   hi('Lf_hl_popup_inputMode', { fg = '#101010', bg = '#cae682', bold = true })
   hi('Lf_hl_popup_category', { fg = '#101010', bg = '#8ac6f2' })
-  hi('Lf_hl_popup_nameOnlyMode', { fg = '#d6d3c8', bg = '#303030' })
-  hi('Lf_hl_popup_fullPathMode', { fg = '#d6d3c8', bg = '#303030' })
-  hi('Lf_hl_popup_regexMode', { fg = '#d6d3c8', bg = '#303030' })
-  hi('Lf_hl_popup_fuzzyMode', { fg = '#d6d3c8', bg = '#303030' })
+  hi('Lf_hl_popup_nameOnlyMode', { fg = 'NvimLightGrey2', bg = '#303030' })
+  hi('Lf_hl_popup_fullPathMode', { fg = 'NvimLightGrey2', bg = '#303030' })
+  hi('Lf_hl_popup_regexMode', { fg = 'NvimLightGrey2', bg = '#303030' })
+  hi('Lf_hl_popup_fuzzyMode', { fg = 'NvimLightGrey2', bg = '#303030' })
   hi('Lf_hl_popup_cwd', { fg = '#404040', bg = '#404040' })
 end
 
